8b32bd61ff7fe4bd4d8033c7d8d0b60c3c8d3348
Qasba <82276586+Qasba@users.noreply.github.com>
2022-02-28 18:15:43 +0530

    Add files via upload

---
+++ set operations AI.py
@@ -0,0 +1,6 @@
+E = {0, 3, 6, 9, 12};
+N = {1, 3, 5, 7, 9};
+print("Union of E and N is:", E | N )
+print("Intersection of E and N is:", E & N)
+print("Difference of E and N is:", E - N)
+print("Symmetric Difference of E and N  is:", E ^ N)
